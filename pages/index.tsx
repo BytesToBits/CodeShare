70dfd24dc8860deb013d8efb3751fc46206015b5
Nemika <mmnemijus@gmail.com>
2022-09-15 14:21:28 +0300

    Stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+import style from "../styles/index.module.scss";
+
 export default function Home() {
   return (
-    <>
-    </>
+    <Box className={style.container}>
+      <Heading
+        fontSize="8xl"
+        fontFamily="Pacifico, cursive"
+        mt="12vh"
+      >
+        CodeShare
+      </Heading>
+
+      <Text
+        fontSize="2xl"
+      >
+        The best way to share code.
+      </Text>
+    </Box>
   )
 }
