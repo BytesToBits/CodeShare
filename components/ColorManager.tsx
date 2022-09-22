fc50785a8612e2d69dbed4fd5c55a14e7c14565c
Nemika__ <mmnemijus@gmail.com>
2022-09-22 17:03:28 +0300

    Removed light mode because it hurts

--- components/ColorManager.tsx
+++ components/ColorManager.tsx
@@ -4,24 +4,28 @@ import React from "react";
 import { FaSun, FaMoon } from "react-icons/fa"
 
 export default function ColorManager(): JSX.Element {
-    const { colorMode, toggleColorMode } = useColorMode();
+    const { colorMode, setColorMode } = useColorMode();
 
-    const iconProps: IconButtonProps = {
-        "aria-label": "Toggle Color Mode",
-        icon: colorMode == "dark" ? <FaSun /> : <FaMoon />,
-        variant: "colorButton",
-        onClick: () => {
-            toggleColorMode();
-            console.log(colorMode);
-        },
-        position: "fixed",
-        bottom: "10px",
-        right: "10px",
-        zIndex: 9999
-    };
+    // const iconProps: IconButtonProps = {
+    //     "aria-label": "Toggle Color Mode",
+    //     icon: colorMode == "dark" ? <FaSun /> : <FaMoon />,
+    //     variant: "colorButton",
+    //     onClick: () => {
+    //         toggleColorMode();
+    //         console.log(colorMode);
+    //     },
+    //     position: "fixed",
+    //     bottom: "10px",
+    //     right: "10px",
+    //     zIndex: 9999
+    // };
 
-    return (
-        <IconButton {...iconProps} />
-    )
+    // return (
+    //     <IconButton {...iconProps} />
+    // )
+
+    if(colorMode == "light") setColorMode("dark")
+
+    return <></>
 
 }
